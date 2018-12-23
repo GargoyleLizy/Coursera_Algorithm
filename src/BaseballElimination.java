dcec4191e7ae518e3ab1a4f6f2bb63d31d3c1048
Zhenya Li <gargoylelizy@gmail.com>
2018-12-23 14:45:38 +0800

    [week3] setup the argument check null methods.

--- src/BaseballElimination.java
+++ src/BaseballElimination.java
@@ -1,6 +1,10 @@
+import edu.princeton.cs.algs4.In;
+import edu.princeton.cs.algs4.StdIn;
+
 public class BaseballElimination {
     // create a baseball division from given filename in format specified below
     public BaseballElimination(String filename) {
+        In in = new In(filename);
 
     }
 
@@ -16,32 +20,45 @@ public class BaseballElimination {
 
     // number of wins for given team
     public int wins(String team) {
+        checkTeamArgumentNull(team, "wins");
         return -1;
     }
 
     // number of losses for given team
     public int losses(String team) {
+        checkTeamArgumentNull(team, "losses");
         return -1;
     }
 
     // number of remaining games for given team
     public int remaining(String team) {
+        checkTeamArgumentNull(team, "remaining");
         return -1;
     }
 
     // number of remaining games between team1 and team2
     public int against(String team1, String team2) {
+        checkTeamArgumentNull(team1, "against 1");
+        checkTeamArgumentNull(team2, "against 2");
         return -1;
     }
 
     // is given team eliminated?
     public boolean isEliminated(String team) {
+        checkTeamArgumentNull(team, "isEliminated");
         return false;
     }
 
     // subset R of teams that eliminates given team; null if not eliminated
     public Iterable<String> certificateOfElimination(String team) {
+        checkTeamArgumentNull(team, "certificateOfElimination");
         return null;
+    }
+
+    private void checkTeamArgumentNull(String team, String method) {
+        if (team == null) {
+            throw new IllegalArgumentException("Argument team should not be null for method: " + method);
+        }
     }
 
 }
